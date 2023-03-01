06800a9c2197d24421401406a5845025a44ea286
unknown <Tarelka1999B@yandex.ru>
2023-03-01 22:24:10 +1000

    Начал делать базовый контент для навигации по странице Диспетчера

--- src/pages/DocPage/Articles/GultureArticles/CultureArticle.tsx
+++ src/pages/DocPage/Articles/GultureArticles/CultureArticle.tsx
@@ -1,10 +1,25 @@
 import ArticleBox from "../../modules/ArticleBox/ArticleBox";
+import "./CultureArticle.css"
 
 const CultureArticle = () => {
     return (
         <ArticleBox>
-            <h1>Про культуру компании</h1>
-            <h2>В нашей компании принято не ходить голым по офису</h2>
+            <div className="ArticleBoxCulture">
+                <div className="ArticleBoxCultureImage">
+                    <div className="ArticleBoxCultureImageItself"></div>
+                </div>
+                <div className="ArticleBoxCultureHeading">
+                    <h2>У нас правило одно – будь красавчиком</h2>
+                </div>
+                <div className="ArticleBoxCultureText">
+                    <p>А остальное узнаешь из Подслушки</p>
+                </div>
+                <div className="ArticleBoxCultureButtonArea">
+                    <button className="ArticleBoxCultureButtonAreaButton">Следующий раздел</button>
+                </div>
+            </div>
+           
+            
         </ArticleBox>
     );
 };
